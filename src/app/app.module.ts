685d13e9086f794df1ae4edc5e1d4b67a39a5c19
admin <mugdhavdeshmukh1998@gmail.com>
2020-04-06 23:06:23 +0530

    created new signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupPageComponent
+    SignupComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
